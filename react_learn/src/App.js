06a92596deb0fb595f42f27e2f968eebf5c7d96c
BogdanRadysuk <radysuk2005@gmail.com>
2023-07-08 16:04:26 +0300

    main

--- react_learn/src/App.js
+++ react_learn/src/App.js
@@ -3,22 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div>
+          <h2>name: Homer</h2>
+          <h2>surname: Simpson</h2>
+          <h3>age: 33</h3>
+          <h3>Sex: Male</h3>
+          <img src="https://static.wikia.nocookie.net/simpsons/images/4/40/I_am_furious_yellow_promo_picture.jpg" alt="homer"/>
+        </div>
+        <div>
+          <h2>name: Marge</h2>
+          <h2>surname: Simpson</h2>
+          <h3>age: 27</h3>
+          <h3>Sex: Female</h3>
+          <img src="https://static.wikia.nocookie.net/simpsons/images/8/8d/Wizard_Marge_Tapped_Out.png" alt="homer"/>
+        </div>
+        <div>
+          <h2>name: Bart</h2>
+          <h2>surname: Simpson</h2>
+          <h3>age: 13</h3>
+          <h3>Sex: Male</h3>
+          <img src="https://static.wikia.nocookie.net/simpsons/images/d/d9/BartmanTSTO.png" alt="homer"/>
+        </div>
+        <div>
+          <h2>name: Lisa</h2>
+          <h2>surname: Simpson</h2>
+          <h3>age: 7</h3>
+          <h3>Sex: Female</h3>
+          <img src="https://static.wikia.nocookie.net/simpsons/images/6/66/Cadet_Lisa_Unlock.png" alt="homer"/>
+        </div>
+        <div>
+          <h2>name: Maggie</h2>
+          <h2>surname: Simpson</h2>
+          <h3>age: 2</h3>
+          <h3>Sex: Female</h3>
+          <img src="https://static.wikia.nocookie.net/simpsons/images/b/b1/Prairie_Maggie.png" alt="homer"/>
+        </div>
+      </>
   );
 }
 
