b294422f40d026add6f40174b50a2a5c1801f143
BogdanRadysuk <radysuk2005@gmail.com>
2023-07-26 16:52:44 +0300

    hw

--- react_learn/src/App.js
+++ react_learn/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import {Posts} from "./components/Posts/Posts";
 import Launches from "./components/Launches/Launches";
 
 function App() {
-  return(
-      // <Posts/>
-      <Launches/>
-  );
+    return (
+        <>
+            <Posts/>
+            <Launches/>
+        </>
+    );
 }
 
 export default App;
